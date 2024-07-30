2ec82505c9d2396b07899d7bef73ad2396ab1af0
sarathi6414 <sarathi.s2312@gmail.com>
2024-07-30 18:06:27 +0530

    Add files via upload

---
+++ jscript.js
@@ -0,0 +1,13 @@
+
+const btn = document.querySelector("#btn");
+
+btn.addEventListener("click", (e) => {
+  const input = document.querySelector("#panNumber");
+  const regex = /^[A-Z]{5}\d{4}[A-Z]$/;
+
+  if (regex.test(input.value)) {
+    alert("The string matches!");
+  } else {
+    alert("give correct PAN no.");
+  }
+});
